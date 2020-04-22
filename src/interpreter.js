1757d06a6b90c35caa54d09c73c40c3cbc11238f
Reginald Braithwaite <reg@braythwayt.com>
2020-04-22 09:29:35 -0400

    integrate parse wwith interpret

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,7 +1,27 @@
 // GÖDEL-NUMBER INTERPRETER
 
-import parse from './parse';
 import { unfoldWith } from './generators';
+
+const seedAndProgramOf = string => string.indexOf('|>') >= 0 ? string.trim().split(/\s*\|>\s*/) : [undefined, string.trim()];
+const seedOf = string => /\d+/.exec(string) ? parseInt(string, 10) : undefined;
+const fractionsOf = string => string.trim().split(
+  /(?:\s|[,:;])+/
+);
+const termPairOf = term => term.split('/').map(
+  str => parseInt(str, 10)
+);
+const termsOf = ([numerator, denominator]) => ({ numerator, denominator });
+
+const parse = syntax => {
+  const [seedSyntax, programSyntax] = seedAndProgramOf(syntax);
+  const seed = seedOf(seedSyntax);
+  const fractions = fractionsOf(programSyntax);
+  const termPairs = fractions.map(termPairOf);
+  const terms = termPairs.map(termsOf);
+
+  return { seed, terms };
+};
+
 
 const transformerOf = ({
     encode, multiply, divide
