d7af746061e291c8013f937a33ef1ee3fd5564d0
Reginald Braithwaite <reg@braythwayt.com>
2020-05-06 07:38:18 -0400

    Update 1.js

--- src/minsky/1.js
+++ src/minsky/1.js
@@ -1,13 +1,25 @@
 import pp from './pp';
 import flatten from './flatten';
-import { parse, interpret } from './interpreter';
+import { parse, interpret, evaluate } from './interpreter';
 
 const evaluateFlattened = (program, ...tapes) => interpret(flatten(parse(program)), tapes);
 
 const marvellousMultiplicationMachine = (
-  '(1,0)/(2,1)→2|(1,0)/(3,1);'      + // decrement #2 by one and goto 2, then erase 3
-  '(1,1)(4,1)/(3,1)|(1,0)/(1,0)→3;' + // move 3 to 1 and 4, then goto 3
-  '(3,1)/(4,1)|(1,0)/(1,0)→1'        // copy 4 back to 3 3, then return to state 1
+  '(1,0)/(2,1)→2|(1,0)/(3,1);      ' + // decrement #2 by one and goto 2, then erase 3
+  '(1,1)(4,1)/(3,1)|(1,0)/(1,0)→3; ' + // move 3 to 1 and 4, then goto 3
+  '(3,1)/(4,1)|(1,0)/(1,0)→1       '   // copy 4 back to 3 3, then return to state 1
+);
+
+const flattenedMultiplicationMachine = (
+  '(1,1)(4,1)(8,1)/(3,1)(7,1) |'  +
+  '(9,1)/(7,1)                |'  +
+  '(3,1)(10,1)/(4,1)(9,1)     |'  +
+  '(1,0)/(9,1)                |'  +
+  '(5,1)/(6,1)                |'  +
+  '(7,1)/(8,1)                |'  +
+  '(9,1)/(10,1)               |'  +
+  '(1,0)(7,1)/(2,1)           |'  +
+  '(1,0)/(3,1)                 '
 );
 
 console.log(pp(
@@ -17,5 +29,11 @@ console.log(pp(
 console.log(JSON.stringify(
   evaluateFlattened(
     marvellousMultiplicationMachine, 0, 3, 13
+  ),
+));
+
+console.log(JSON.stringify(
+  evaluate(
+    flattenedMultiplicationMachine, 0, 3, 13
   ),
 ));
